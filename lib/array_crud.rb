dbd6952d6481e9ea6bf97b42047738beafd8e42a
Adam Ervin <adamjervin@gmail.com>
2019-06-04 23:49:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-add_element_to_end_of_array =  ["panther" , "panthy"]
-  add_element_to_start_of_array << "snake"
+
 end
 
 def add_element_to_start_of_array(array, element)
