b345983e53ef2cc345007ff38d7e4e59a4b43ccf
Adam Ervin <adamjervin@gmail.com>
2019-06-04 23:58:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
- array = ["im", "terrible", "at", "coding", "ruby"]
+ array = "terrible"
     element = "struggle"
     add_element_to_start_of_array.push("element")
 end
