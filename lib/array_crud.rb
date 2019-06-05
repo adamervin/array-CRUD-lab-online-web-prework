d8123032e4a0cb98957769b21232c320144f19ad
Adam Ervin <adamjervin@gmail.com>
2019-06-05 00:14:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    lame = "my code"
-    me = "scrub"
+    lame = "my code", "my life"
     add_element_to_start_of_array << "ruby"
      
 end
